bd371de178b2c42ae5e38b3e30fabe3a1b063b2d
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2023-01-23 14:21:26 -0300

    feat: get tweets by username

--- src/main/java/com/tweetero/api/controllers/TweetController.java
+++ src/main/java/com/tweetero/api/controllers/TweetController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -60,6 +61,25 @@ public class TweetController {
             data.put("avatar", i.getUser().getAvatar());
 
             formatedList.add(data);
+        });
+
+        return formatedList;
+    }
+
+    @GetMapping("/{username}")
+    public List<Map<Object, Object>> getTweetsByUsername(@PathVariable String username) {
+        List<Tweet> list = tweetRepository.findAll();
+        List<Map<Object, Object>> formatedList = new ArrayList<>();
+
+        list.forEach(i -> {
+            if (i.getUser().getUsername().equals(username)) {
+                Map<Object, Object> data = new HashMap<>();
+                data.put("tweet", i.getTweet());
+                data.put("username", i.getUser().getUsername());
+                data.put("avatar", i.getUser().getAvatar());
+
+                formatedList.add(data);
+            }
         });
 
         return formatedList;
